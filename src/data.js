4d42bbe29839637f7153be380cb36b2f150c8ebf
Thomas Hurrie <thomas.hurrie@gmail.com>
2021-06-24 19:58:23 -0400

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,7 @@
+export const names = [
+  { personname: "Heather", id: "0001" },
+  { personname: "Nolan", id: "0002" },
+  { personname: "Colin", id: "0003" },
+  { personname: "Sile", id: "0004" },
+  { personname: "Mark", id: "0005" }
+];
